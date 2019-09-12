c305f9fafe411057be6da5293248f3f15e0ff9ce
felipecarmonaucr <luis.carmonacastro@ucr.ac.cr>
2019-09-12 09:57:01 -0600

    Basic packing and unpacking functions were implemented

--- Codigo/Protocolo/server.py
+++ Codigo/Protocolo/server.py
@@ -1,27 +1,22 @@
 import socket
 from typing import NamedTuple
-from time import time
-from struct import *
-import pickle
+import time
+import struct
 
+values = []
 
-class MessageClass(NamedTuple):
-	randomID: int
-	date: bytes
-	sensorId: int
-	sensorType: str
-	boolData: bool
-	floatData: float
+#BiiBf
 	
+#UDP_IP = "10.1.137.90"
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
-
+s = struct.Struct("BIBBBBBf")
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    MessageClass1 = pickle.loads(data)
-    print(str(unpack(">B",MessageClass1.randomID)) + str(unpack(">B",MessageClass1.sensorId)) + str(MessageClass1.sensorType))
-sock.sendto((MessageClass1.randomID), addr)
+    values = s.unpack(data)
+    dtime = time.ctime(values[1])
+    print(str(values[0]) + " " + str(dtime) + " " + str(values[2]) + " " + str(values[3]) + " " + str(values[4]) + " " + str(values[5]) + " " + str(values[6]) + " " + str(values[7]))
