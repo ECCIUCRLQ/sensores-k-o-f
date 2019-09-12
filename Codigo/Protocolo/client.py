b2b067c775e85d01bcad1b6400ab62387a5e883c
felipecarmonaucr <luis.carmonacastro@ucr.ac.cr>
2019-09-12 10:45:20 -0600

    ACK implemented and workin

--- Codigo/Protocolo/client.py
+++ Codigo/Protocolo/client.py
@@ -1,9 +1,9 @@
 import socket
 from typing import NamedTuple
 from time import time
+#import time
 import struct
 import random
-
 
 sensorId = [0x00,0x01,0x02]
 #values = (randomId, date, sensorId, sensorType, data)
@@ -16,9 +16,9 @@ values.append(0)
 
 s = struct.Struct('BIBBBBBf')
 pack = s.pack(values[0],values[1],values[2],sensorId[0],sensorId[1],sensorId[2],values[3],values[4])
-UDP_IP = "127.0.0.1"
-#UDP_IP = "10.1.137.67"
-UDP_PORT = 5005
+#UDP_IP = "127.0.0.1"
+UDP_IP = "10.1.137.102"
+UDP_PORT = 10000
 MESSAGE = pack
 
 print ("UDP target IP:", UDP_IP)
@@ -29,5 +29,5 @@ sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
-#msm = sock.recvfrom(1024)
-#print(msm)
+msm, addr = sock.recvfrom(1024)
+print(str(msm[0])+ " " + str(msm[1]) + " " + str(msm[2]) + " " + str(msm[3]))
--- Codigo/Protocolo/server.py
+++ Codigo/Protocolo/server.py
@@ -7,16 +7,19 @@ values = []
 
 #BiiBf
 	
-#UDP_IP = "10.1.137.90"
-UDP_IP = "127.0.0.1"
+UDP_IP = "10.1.137.90"
+#UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 s = struct.Struct("BIBBBBBf")
+ss = struct.Struct("BBBBB")
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     values = s.unpack(data)
     dtime = time.ctime(values[1])
     print(str(values[0]) + " " + str(dtime) + " " + str(values[2]) + " " + str(values[3]) + " " + str(values[4]) + " " + str(values[5]) + " " + str(values[6]) + " " + str(values[7]))
+    msm = ss.pack(values[0],values[2],values[3],values[4],values[5])
+    sock.sendto(msm, addr)
