3969c974906b1d61f4b8d720e72b5a9a05a382a8
KevinBarrantes <kbarransa@hotmail.com>
2019-11-21 10:49:45 -0600

    Implementation to save files added to filesystem.py

--- Codigo/File_System/filesystem.py
+++ Codigo/File_System/filesystem.py
@@ -1,7 +1,7 @@
 #File system code. This file system configures a node to save n bytes
 import sys 
 import math
-import time
+from time import time
 import socket
 
 memory_size = 0
@@ -35,15 +35,23 @@ if (len(sys.argv)) == 2:
 		#In case that an operation is received
 		if readable[0]:
 			data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-			if(data[0] == 0):
+			if(data[0] == 0): #Expected structure: Operation, ID_Page, Page_Size, Data
+				#Meta structure: Page_ID, Size, Offset, Creation_Date, Consult_Date
 				f = open("bin.txt", "rb+")
 				f.seek(memory_meta)
 				f.write((data[1]).to_bytes(1, "big"))
-				f.write((data[1]).to_bytes(1, "big"))
-				file_size = stuct.unpack("I", data[2:6])
+				file_size = struct.unpack("I", data[2:6])
 				f.write((file_size).to_bytes(4, "big"))
+				offset = memory_size - file_size
+				f.write((offset).to_bytes(4, "big"))
+				f.write((int(time.time())).to_bytes(4, "big")) #Creation_Date
+				f.write((int(time.time())).to_bytes(4, "big")) #Mod_Date
+				memory_meta += 17
+				f.seek(offset)
+				for i in range(6:(file_size + 5)):
+					f.write((data[i]).to_bytes(1, "big"))
 				f.close()
-				memory_size -= 8 #change this to package size
+				memory_size = offset #change this to package size
 				msm = memory_size - 4
 				#sock.sendto(msm, (addr)) returns available memory
 				
